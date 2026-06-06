c4f9460111b40b04669fdaaca186b751eb9be7e9
Tanvi Kule <tanvikule5@gmail.com>
2026-06-06 21:22:29 +0530

    setup backend architecture and express server

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({ message: "MiniCart Backend Running 🚀" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
