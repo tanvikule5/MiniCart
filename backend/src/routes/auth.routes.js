f723fc08ea945a2fe4730635d3635a03fde9a1a4
Tanvi Kule <tanvikule5@gmail.com>
2026-06-10 16:54:20 +0530

    Implement JWT authentication and route protection

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,14 @@
 import express from "express";
 import { registerUser , loginUser} from "../controllers/auth.controller.js";
-
+import authMiddleware from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", authMiddleware, (req, res) => {
+  res.status(200).json({
+    success: true,
+    user: req.user
+  });
+});
 export default router;
