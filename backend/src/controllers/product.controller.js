4694786a6961d1759b87ba7706e26d78dec5f607
Tanvi Kule <tanvikule5@gmail.com>
2026-06-24 18:40:31 +0530

    Complete product CRUD functionality

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import prisma from "../config/prisma.js";
+//create product
 export const createProduct = async (req, res) => {
   try {
     const {
@@ -49,6 +50,198 @@ export const createProduct = async (req, res) => {
       success: true,
       message: "Product created successfully",
       product
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+//getallproducts
+  export const getAllProducts = async (req, res) => {
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+  seller: {
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      phoneNumber: true,
+      department: true,
+      year: true,
+      profileImage: true
+    }
+  },
+  category: true
+}
+    });
+
+    return res.status(200).json({
+      success: true,
+      count: products.length,
+      products
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+  };
+
+  //getproductbyid
+  export const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await prisma.product.findUnique({
+      where: { id },
+      include: {
+        seller: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            phoneNumber: true,
+            department: true,
+            year: true,
+            profileImage: true
+          }
+        },
+        category: true
+      }
+    });
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found"
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      product
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+//getMyProducts
+export const getMyProducts = async (req, res) => {
+  try {
+
+    const sellerId = req.user.userId;
+
+    const products = await prisma.product.findMany({
+      where: {
+        sellerId
+      },
+      include: {
+        category: true
+      }
+    });
+
+    return res.status(200).json({
+      success: true,
+      count: products.length,
+      products
+    });
+
+  } catch (error) {
+
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+
+  }
+};
+//update product
+export const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Find product
+    const product = await prisma.product.findUnique({
+      where: { id }
+    });
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found"
+      });
+    }
+
+    // Check ownership
+    if (product.sellerId !== req.user.userId) {
+      return res.status(403).json({
+        success: false,
+        message: "Unauthorized"
+      });
+    }
+
+    // Update product
+    const updatedProduct = await prisma.product.update({
+      where: { id },
+      data: req.body
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Product updated successfully",
+      updatedProduct
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+//delete product
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Find product
+    const product = await prisma.product.findUnique({
+      where: { id }
+    });
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found"
+      });
+    }
+
+    // Check ownership
+    if (product.sellerId !== req.user.userId) {
+      return res.status(403).json({
+        success: false,
+        message: "Unauthorized"
+      });
+    }
+
+    // Delete product
+    await prisma.product.delete({
+      where: { id }
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted successfully"
     });
 
   } catch (error) {
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { createProduct } from "../controllers/product.controller.js";
+import { createProduct,getAllProducts,  getProductById , getMyProducts, updateProduct, deleteProduct} from "../controllers/product.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/create", authMiddleware, createProduct);
+//getallproducts
+router.get("/",getAllProducts);
+
+// get products of logged-in user
+router.get("/my/products", authMiddleware, getMyProducts);
+//update product
+router.put("/:id", authMiddleware, updateProduct);
+//delete product
+router.delete("/:id", authMiddleware, deleteProduct);
+// get product by id
+router.get("/:id", getProductById);
 
 export default router;
